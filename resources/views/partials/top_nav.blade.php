a3274e10210ffbb64400262aa603b3c9e552c971
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-04-06 13:16:03 -0500

    Update top_nav.blade.php
    Configuración Inicial

--- resources/views/partials/top_nav.blade.php
+++ resources/views/partials/top_nav.blade.php
@@ -1,8 +1,11 @@
 <nav class="top-nav" x-data="{ expanded: false }" x-bind:class="expanded && 'mobile'">
     <div class="top-nav__left">
-        <a class="top-nav__branding" href="{{ route('home.index') }}">
-            <i class="fal fa-tv-retro"></i>
-            <span class="top-nav__site-logo">{{ \config('other.title') }}</span>
+        <!--<a class="top-nav__branding" href="{{ route('home.index') }}">-->
+            <!--<i class="fal fa-tv-retro"></i>-->
+            <!--<span class="top-nav__site-logo">{{ \config('other.title') }}</span>-->
+        
+            <a href="{{ route('home.index') }}"><img src="{{ url('/img/ltsmall.png') }}">
+        
         </a>
         <livewire:quick-search-dropdown />
     </div>
