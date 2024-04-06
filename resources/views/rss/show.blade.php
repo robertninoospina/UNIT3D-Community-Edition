f24849ed60709f65c9248233116cfd481b8352dd
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-04-06 13:17:50 -0500

    Update show.blade.php
    Configuración Inicial

--- resources/views/rss/show.blade.php
+++ resources/views/rss/show.blade.php
@@ -1,5 +1,6 @@
 @php
-    echo '<?xml version="1.0" encoding="UTF-8" ?>'
+    echo '<?xml version="1.0" encoding="UTF-8" ?>';
+    $poster='';
 @endphp
 <rss version="2.0"
      xmlns:atom="http://www.w3.org/2005/Atom"
@@ -18,62 +19,80 @@
         <lastBuildDate>{{ now()->toRssString() }}</lastBuildDate>
         <ttl>5</ttl>
         @if($torrents)
-            @foreach($torrents as $torrent)
+            @foreach($torrents as $data)
+            	@php
+                        $meta = match (true) {
+                            $data->category->tv_meta => App\Models\Tv::query()
+                                ->with('genres', 'networks', 'seasons')
+                                ->find($data->tmdb ?? 0),
+                            $data->category->movie_meta => App\Models\Movie::query()
+                                ->with('genres', 'companies', 'collection')
+                                ->find($data->tmdb ?? 0),                            
+                            default => null,
+                        };
+                 @endphp
                 <item>
-                    <title>{{ $torrent->name }}</title>
-                    <category>{{ $torrent->category->name }}</category>
-                    <contentlength>{{ $torrent->size }}</contentlength>
-                    <link>{{ route('torrent.download.rsskey', ['id' => $torrent->id, 'rsskey' => $user->rsskey ]) }}</link>
-                    <guid>{{ $torrent->id }}</guid>
+                    <title>{{ $data->name }}</title>                    
+                    <category>{{ $data->category->name }}</category>
+                    <type>{{ $data->type->name }}</type>
+		    <resolution>{{ $data->resolution->name ?? 'No Res' }}</resolution>
+	   	    <size>{{ $data->getSize() }}</size>
+		    <imdb>
+			@if (($data->category->movie_meta || $data->category->tv_meta) && $data->imdb != 0)
+				https://anon.to?http://www.imdb.com/title/tt{{ $data->imdb }};                        
+                        @endif
+		     </imdb>
+                     <poster>
+			@php
+				$poster='https://upload.wikimedia.org/wikipedia/commons/b/b9/No_Cover.jpg';
+				if ($data->category->movie_meta && $data->tmdb != 0)
+					$poster = tmdb_image('poster_big', $meta->poster);
+				if ($data->category->tv_meta && $data->tmdb != 0) 
+                                	$poster = tmdb_image('poster_big', $meta->poster); 
+				if (!$data->category->movie_meta && !$data->category->tv_meta)
+					if(file_exists(public_path().'/files/img/torrent-cover_'.$data->id.'.jpg'))
+						$poster = url('files/img/torrent-cover_' . $data->id . '.jpg');
+
+			@endphp
+			{{$poster}}
+		     </poster>
+		     <tmdb>
+				@if ($data->category->movie_meta && $data->tmdb != 0)
+					https://anon.to?https://www.themoviedb.org/movie/{{ $data->tmdb }}
+                        	@elseif ($data->category->tv_meta && $data->tmdb != 0)
+					https://anon.to?https://www.themoviedb.org/tv/{{ $data->tmdb }}                        	
+                        	@endif
+		    </tmdb>
+		    <link>{{ route('torrents.show', ['id' => $data->id ]) }}</link>                    
+                    <guid>{{ $data->id }}</guid>
                     <description>
-                        <![CDATA[<p>
-                            <strong>Name</strong>: {{ $torrent->name }}<br>
-                            <strong>Category</strong>: {{ $torrent->category->name }}<br>
-                            <strong>Type</strong>: {{ $torrent->type->name }}<br>
-                            <strong>Resolution</strong>: {{ $torrent->resolution->name ?? 'No Res' }}<br>
-                            <strong>Size</strong>: {{ $torrent->getSize() }}<br>
-                            <strong>Uploaded</strong>: {{ $torrent->created_at->diffForHumans() }}<br>
-                            <strong>Seeders</strong>: {{ $torrent->seeders }} |
-                            <strong>Leechers</strong>: {{ $torrent->leechers }} |
-                            <strong>Completed</strong>: {{ $torrent->times_completed }}<br>
-                            <strong>Uploader</strong>:
-                            @if(!$torrent->anon && $torrent->user)
-                                {{ __('torrent.uploaded-by') }} {{ $torrent->user->username }}
-                            @else
-                                {{ __('common.anonymous') }} {{ __('torrent.uploader') }}
-                            @endif<br>
-                            @if (($torrent->category->movie_meta || $torrent->category->tv_meta) && $torrent->imdb != 0)
-                                IMDB Link:<a href="https://anon.to?http://www.imdb.com/title/tt{{ $torrent->imdb }}"
-                                             target="_blank">tt{{ $torrent->imdb }}</a><br>
-                            @endif
-                            @if ($torrent->category->movie_meta && $torrent->tmdb != 0)
-                                TMDB Link: <a href="https://anon.to?https://www.themoviedb.org/movie/{{ $torrent->tmdb }}"
-                                              target="_blank">{{ $torrent->tmdb }}</a><br>
-                            @elseif ($torrent->category->tv_meta && $torrent->tmdb != 0)
-                                TMDB Link: <a href="https://anon.to?https://www.themoviedb.org/tv/{{ $torrent->tmdb }}"
-                                              target="_blank">{{ $torrent->tmdb }}</a><br>
-                            @endif
-                            @if (($torrent->category->tv_meta) && $torrent->tvdb != 0)
-                                TVDB Link:<a href="https://anon.to?https://www.thetvdb.com/?tab=series&id={{ $torrent->tvdb }}"
-                                             target="_blank">{{ $torrent->tvdb }}</a><br>
-                            @endif
-                            @if (($torrent->category->movie_meta || $torrent->category->tv_meta) && $torrent->mal != 0)
-                                MAL Link:<a href="https://anon.to?https://myanimelist.net/anime/{{ $torrent->mal }}"
-                                             target="_blank">{{ $torrent->mal }}</a><br>
-                            @endif
-                            @if ($torrent->internal == 1)
-                                <comments>This is a high quality internal release!</comments>
-                            @endif
-                        </p>]]>
-                    </description>
+                    	{{$poster}} 
+		    	Categoria: {{ $data->category->name }}		    
+		    	Tipo: {{ $data->type->name }}
+		    	Resolucion: {{ $data->resolution->name ?? 'No Res' }}
+		    	Tama&#328;o: {{ $data->getSize() }}
+ 		    	@if (($data->category->movie_meta || $data->category->tv_meta)  && $data->imdb != 0)
+		    		[![](https://icons.iconarchive.com/icons/danleech/simple/48/imdb-icon.png)](http://www.imdb.com/title/tt{{ $data->imdb }})
+ 		     	@endif
+		     	@if ($data->category->movie_meta && $data->tmdb != 0)
+				[![](https://i.ibb.co/q0LP1H6/tmdbsmall.png)](https://www.themoviedb.org/movie/{{ $data->tmdb }})
+                        @elseif ($data->category->tv_meta && $data->tmdb != 0)
+				[![](https://i.ibb.co/q0LP1H6/tmdbsmall.png)](https://www.themoviedb.org/tv/{{ $data->tmdb }}) 
+			@endif
+		   </description>
                     <dc:creator xmlns:dc="http://purl.org/dc/elements/1.1/">
-                        @if(!$torrent->anon && $torrent->user)
-                            {{ __('torrent.uploaded-by') }} {{ $torrent->user->username }}
+                        @if(!$data->anon && $data->user)
+                            {{ __('torrent.uploaded-by') }} {{ $data->user->username }}
                         @else
                             {{ __('common.anonymous') }} {{ __('torrent.uploader') }}
                         @endif
                     </dc:creator>
-                    <pubDate>{{ $torrent->created_at->toRssString() }}</pubDate>
+                    <pubDate>{{ $data->created_at->toRssString() }}</pubDate>
+                    <enclosure
+                            url="{{ route('torrent.download.rsskey', ['id' => $data->id, 'rsskey' => $user->rsskey ]) }}"
+                            type="application/x-bittorrent"
+                            length="39399"
+                    />
                 </item>
             @endforeach
         @endif
