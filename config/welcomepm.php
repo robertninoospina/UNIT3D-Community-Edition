43954ede995b8e6ad474cfd289385eaf54b9109d
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-04-06 13:02:33 -0500

    Update welcomepm.php
    Configuración inicial

--- config/welcomepm.php
+++ config/welcomepm.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'subject' => 'Welcome to UNIT3D',
+    'subject' => 'Bienvenido a Lat-Team',
 
     /*
     |--------------------------------------------------------------------------
@@ -32,7 +32,7 @@ return [
     |
     */
 
-    'message' => 'Welcome To [b]UNIT3D[/b].
+    'message' => 'Bienvenido a [b]Lat-Team[/b].
 
-    - UNIT3D :space_invader:',
+    - Lat-Team :space_invader:',
 ];
