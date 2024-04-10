bbc577d9e6036d8a9e274e9f28bced2609d46e72
robertninoospina <166242738+robertninoospina@users.noreply.github.com>
2024-04-10 11:56:34 -0500

    Update two-factor-challenge.blade.php

--- resources/views/auth/two-factor-challenge.blade.php
+++ resources/views/auth/two-factor-challenge.blade.php
@@ -42,7 +42,8 @@
                     @csrf
                     <a class="auth-form__branding" href="{{ route('home.index') }}">
                         <i class="fal fa-tv-retro"></i>
-                        <span class="auth-form__site-logo">{{ \config('other.title') }}</span>
+                        <img class="auth-form__site-logo-lateam" src="{{ url('/img/logo.png') }}" alt="LaTeam" />
+                        <!--<span class="auth-form__site-logo">{{ \config('other.title') }}</span>-->
                     </a>
                     <ul class="auth-form__important-infos">
                         <li class="auth-form__important-info" x-show="!recovery">
@@ -112,6 +113,14 @@
                             @endforeach
                         </ul>
                     @endif
+
+                    <div class="discord-div">
+                    <a class="discord-widget" href="https://discord.gg/RUKj5JfEST" title="Join us on Discord">
+                        <img src="https://discordapp.com/api/guilds/838217297478680596/embed.png?style=banner3">
+                    </a>
+                </div>
+
+                    
                 </form>
             </section>
         </main>
