1b512b3eb19d4d25d3209219b4813cb239e3ab2a
Robert <robertfenyinernino@gmail.com>
2024-06-23 10:55:45 -0500

    Sync

--- config/other.php
+++ config/other.php
@@ -21,7 +21,7 @@ return [
     |
     */
 
-    'title' => 'Lat-Team INSTALACION DE PRUEBAS',
+    'title' => 'Lat-Team ESPEJO',
 
     /*
     |--------------------------------------------------------------------------
